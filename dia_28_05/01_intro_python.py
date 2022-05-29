5d0862d4674c2620317e10108edd3d7c178c946a
josedembo <josedembo18@aluno.unilab.edu.br>
2022-05-29 08:12:50 -0300

    Cria a pasta de conteudos do dia 28
    move os arquvos do dia 28 para a pasta do dia 28

---
+++ dia_28_05/01_intro_python.py
@@ -0,0 +1,21 @@
+print("Hello, world")
+print("Real Madrid Campião")
+a = 20
+b = 30.6
+
+# o codido abaixo imprime
+# os valores das varias 
+# a e b
+
+""" 
+O codigo abaixo imprime 
+na tela os valores 
+das variáveis a e b
+"""
+print(a)
+print(b)
+
+# imprime o tipo da variavel na tela 
+# print(type(a))
+print(type(b))
+
---
+++ dia_28_05/main.py
@@ -0,0 +1,14 @@
+nome = input("digite o seu nome: ")
+
+a = int(input("digite um número: "))
+b = int(input("digite outro número: "))
+soma = a + b 
+
+print("o seu nome é", nome)
+print("vocé digitou o número", a)
+print("você digitou também o número", b)
+
+print("A soma é igual a: ", soma)
+
+print(type(a))
+print(type(b))
