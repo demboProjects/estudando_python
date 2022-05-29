5f1a1054b2574a8507f26ea93ff3adcff62a55cc
josedembo <josedembo18@aluno.unilab.edu.br>
2022-05-29 08:15:08 -0300

    Cria a pasta de ecercicios
    Adciona os Exercicios resolvidos do dia 28

---
+++ exercicios/dia_28_05_22/exerc_03.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que peça dois números e imprima a soma.
+"""
+
+numero_1 = int(input("digite um número: "))
+numero_2 = int(input("digite outro número"))
+
+soma = numero_1 + numero_2
+
+print("A soma entre os dois números digitados é: ", soma)
---
+++ exercicios/dia_28_05_22/exerc_05.py
@@ -0,0 +1,8 @@
+"""
+Faça um Programa que converta metros para centímetros.
+"""
+valor_metro = int(input("Digite o valor em metros: "))
+
+valor_centimetro = valor_metro * 100
+
+print("Valor convertido em centímetros: ", valor_centimetro)
