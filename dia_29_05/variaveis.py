4ac1eba8db4c72b19d763379448c7f0d6d96931e
josedembo <josedembo18@aluno.unilab.edu.br>
2022-05-29 22:13:58 -0300

    adiciona o conteudo do dia 29

---
+++ dia_29_05/variaveis.py
@@ -0,0 +1,42 @@
+_nome1 = "Sumaé"
+nomedealuno = "aluno1"
+
+# camel Case
+nomeDeAluno = "auno1"
+
+# Pascal Case
+NomeDeAluno = "aluno2"
+
+# snick case (caso de cobra)
+nome_de_aluno = "aluno3"
+
+# atribuição de multiplas variaveis 
+x, y, z = 2, 3, 4
+
+print(x)
+print(y)
+print(z)
+
+# Atribuindo um único valor para várias variaveis
+a = b = c = 20
+
+print(a)
+print(b)
+print(c)
+
+# descompatando variaveis
+frutas = ["apple", "banana", "cherry"]
+x, y, z = frutas
+print(x)
+print(y)
+print(z)
+
+notas = [9.0, 7.5, 5.0, 10.0]
+
+nota1, nota2, nota3, nota4  = notas
+print("nota 1: ", nota1)
+print("nota 2: ", nota2)
+print("nota 3: ", nota3)
+print("nota 4: ", nota4)
+
+#print(_nome1)
---
+++ exercicios/dia_29_05/exerc_6.py
@@ -0,0 +1,10 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+formula -> A = π r²
+"""
+
+raio = float(input("Digite o valor do raio do circulo: "))
+
+area = 3.14 * (raio ** 2)
+
+print("O valor da área do circulo é: ", area)
